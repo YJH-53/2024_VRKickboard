ede9680b4eecedfa9dce8510656b63041cefbbef
YJH-53 <164444889+YJH-53@users.noreply.github.com>
2024-08-06 15:06:16 +0900

    Scene1 Added
    사용방법: Course Scene -> Scene1에서 1인칭 스쿠터랑 감점 시 뜨는 layer인 DamageGo 사용해주셈
    Scripts 파일 자체를 재구성했고 Camera랑 Layer 띄우는 script만 추가했으니 그냥 복붙 ㄱㄱ

---
+++ Assets/Ash Assets/Arcade Bike Physics/Scripts/DamageEffect/TakeDamage.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class TakeDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float intensity = 0;
+    PostProcessVolume _volume;
+    Vignette _vignette;
+
+    void Start()
+    {
+        _volume = GetComponent<PostProcessVolume>();
+        _volume.profile.TryGetSettings<Vignette>(out _vignette);
+        if(!_vignette){
+            print("error, vignette empty");
+        }else{
+            _vignette.enabled.Override(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Damage Layer이 뜨는 상황을 설정하는 부분
+        if(Input.GetMouseButtonDown(0))
+        StartCoroutine(TakeDamageEffect());
+    }
+
+    private IEnumerator TakeDamageEffect()
+    {
+        intensity = 0.4f;
+        _vignette.enabled.Override(true);
+        _vignette.intensity.Override(0.4f);
+
+        yield return new WaitForSeconds(0.4f);
+        
+        while(intensity > 0){
+            intensity -= 0.04f;
+            if(intensity < 0) intensity = 0;
+            _vignette.intensity.Override(intensity);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        _vignette.enabled.Override(false);
+        yield break;
+    }
+}
